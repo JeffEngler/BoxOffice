13d27c435818676b92d2c1cb12c181061c16ce43
JeffEngler <54863910+JeffEngler@users.noreply.github.com>
2019-09-03 19:05:41 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BoxOffice
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.AddBandBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RemoveBandBTN = new System.Windows.Forms.Button();
+            this.TotalCountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(46, 27);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(569, 359);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // AddBandBTN
+            // 
+            this.AddBandBTN.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.AddBandBTN.Location = new System.Drawing.Point(138, 392);
+            this.AddBandBTN.Name = "AddBandBTN";
+            this.AddBandBTN.Size = new System.Drawing.Size(122, 23);
+            this.AddBandBTN.TabIndex = 1;
+            this.AddBandBTN.Text = "Add Band";
+            this.AddBandBTN.UseVisualStyleBackColor = true;
+            this.AddBandBTN.Click += new System.EventHandler(this.AddBandBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to Box Office!";
+            // 
+            // RemoveBandBTN
+            // 
+            this.RemoveBandBTN.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.RemoveBandBTN.Location = new System.Drawing.Point(10, 392);
+            this.RemoveBandBTN.Name = "RemoveBandBTN";
+            this.RemoveBandBTN.Size = new System.Drawing.Size(122, 23);
+            this.RemoveBandBTN.TabIndex = 3;
+            this.RemoveBandBTN.Text = "Remove Band";
+            this.RemoveBandBTN.UseVisualStyleBackColor = true;
+            this.RemoveBandBTN.Click += new System.EventHandler(this.RemoveBandBTN_Click);
+            // 
+            // TotalCountLabel
+            // 
+            this.TotalCountLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.TotalCountLabel.AutoSize = true;
+            this.TotalCountLabel.Location = new System.Drawing.Point(506, 397);
+            this.TotalCountLabel.Name = "TotalCountLabel";
+            this.TotalCountLabel.Size = new System.Drawing.Size(95, 17);
+            this.TotalCountLabel.TabIndex = 4;
+            this.TotalCountLabel.Text = "Total count: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 453);
+            this.Controls.Add(this.TotalCountLabel);
+            this.Controls.Add(this.RemoveBandBTN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddBandBTN);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(675, 1000);
+            this.MinimumSize = new System.Drawing.Size(675, 150);
+            this.Name = "Form1";
+            this.Text = "Box Office";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button AddBandBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RemoveBandBTN;
+        private System.Windows.Forms.Label TotalCountLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BoxOffice
+{
+    /// <summary>
+    /// The purpose of this form is to keep track of the number of attendees coming into the venue
+    /// to see each band, and to add up the total attendence for the night. 
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        //index number used through out the program to match corresponding controls
+        public int i = 0;
+
+        /// <summary>
+        /// default constructor 
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Static Button Methods
+        //These Methods provide functionality to the "Add Band" and "Remove Band buttons"
+
+
+        /// <summary>
+        /// Adds all the controls needed to keep track of attendees coming to see a band
+        /// </summary>
+        public void AddBandBTN_Click(object sender, EventArgs e)
+        {
+            //increment the index number
+            i++;
+
+            CreateBandNameTextBox();
+            CreateSub1Button();
+            CreatCountTextBox();
+            CreateAdd1Button();
+        }
+
+        /// <summary>
+        /// Removes all the controls needed to keep track of attendees coming to see a band
+        /// </summary>
+        private void RemoveBandBTN_Click(object sender, EventArgs e)
+        {
+            //check to prevent out of range error
+            if (flowLayoutPanel1.Controls.Count < 4)
+                return;
+
+            //remove the 4 most recently created elements, becuase there are 4 elements for each band
+            for (int j = 0; j < 4; j++)
+            {
+                flowLayoutPanel1.Controls.RemoveAt(flowLayoutPanel1.Controls.Count - 1);
+            }
+            //decrement index
+            i--;
+
+            //calculate and display total attendence 
+            TotalCountLabel.Text = Total();
+        }
+        #endregion
+
+        #region Dynamic Button Methods
+        // These Methods provide functionality to the "-1" and "+1" buttons
+
+
+        /// <summary>
+        /// subtracts 1 from the number of attendees for a band
+        /// </summary>
+        private void Sub1_click(object sender, EventArgs e)
+        {
+            //create new button variable, and assign the button that called the function to the new varialble
+            Button Btn = (Button)sender;
+            //subtract 1 from the corresponding count 
+            AddOrSubtract(Btn);
+        }
+
+        /// <summary>
+        /// subtracts 1 from the number of attendees for a band
+        /// </summary>
+        private void Add1_click(object sender, EventArgs e)
+        {
+            //create new button variable, and assign the button that called the function to the new varialble
+            Button Btn = (Button)sender;
+            //add 1 to the corresponding count
+            AddOrSubtract(Btn);
+        }
+        #endregion
+
+        #region "Add Band" Helper Methods
+        //These methods are called by the AddBandBTN_Click method
+
+
+        private void CreateBandNameTextBox()
+        {
+            //create a textbox for the user to enter the name of a band
+            TextBox BandName = new TextBox();
+
+            //add the index number to the end of the textbox name
+            BandName.Name = $"BandName { i }";
+
+            //set properties for the textbox
+            BandName.Text = "Enter Band Name";
+            BandName.Width = 156;
+            Padding margin = BandName.Margin;
+            margin.Right = 119;
+            BandName.Margin = margin;
+
+            //add textbox to the GUI via flowlayoutpanel
+            flowLayoutPanel1.Controls.Add(BandName);
+        }
+
+        private void CreateSub1Button()
+        {
+            //create a button to decrement the count of attendees for a band
+            Button Sub1 = new Button();
+
+            //add the index number to the end of the name
+            Sub1.Name = $"Sub1_ { i }";
+
+            //set properties
+            Sub1.Text = "-1";
+            Sub1.Width = 50;
+
+            //call function when clicked
+            Sub1.Click += new EventHandler(Sub1_click);
+
+            //add to the GUI via flowlayoutpanel
+            flowLayoutPanel1.Controls.Add(Sub1);
+        }
+
+        private void CreatCountTextBox()
+        {
+            //create a textbox for the count of attendees for a band
+            TextBox Count = new TextBox();
+
+            //add the index number to the end of the name
+            Count.Name = $"Count { i }";
+
+            //set properties
+            Count.Text = "0";
+            Count.Width = 30;
+            Count.TextAlign = HorizontalAlignment.Center;
+
+            //add to the GUI via flowlayoutpanel
+            flowLayoutPanel1.Controls.Add(Count);
+        }
+
+        private void CreateAdd1Button()
+        {
+            //create button to increment the count of attendees for a band 
+            Button Add1 = new Button();
+
+            //add the index number to the end of the name
+            Add1.Name = $"Add1_ { i }";
+
+            // set properties
+            Add1.Text = "+1";
+            Add1.Width = 50;
+
+            //call function when clicked
+            Add1.Click += new EventHandler(Add1_click);
+
+            //add to the GUI via flowlayoutpanel
+            flowLayoutPanel1.Controls.Add(Add1);
+        }
+        #endregion
+
+        #region Other Helper Methods
+        // the AddOrSubtract, Total, and InputCheck Methods called by other methods can be found here 
+
+
+        /// <summary>
+        /// Adds or Subtracts from the corresponding count, depending on which button was clicked
+        /// </summary>
+        /// <param name="Btn"></param>
+        private void AddOrSubtract(Button Btn)
+        {
+            //use the index number attatched to the button name to find the corresponding 'count' textbox. 
+            TextBox Count = (TextBox)flowLayoutPanel1.Controls.Find($"Count { Btn.Name[Btn.Name.Length - 1] }", false).FirstOrDefault();
+            //if there is invalid input in the count box, reset the value and notify the user
+            if (InputCheck(Count) == false)
+                return;
+            //checks to see if the button clicked was the subtract button
+            if (Btn.Text == "-1")
+            {
+                //prevent the count from becoming negative
+                if (int.Parse(Count.Text) == 0)
+                    return;
+                //convert the count text from string to int, subtract 1, then convert back to string
+                Count.Text = (int.Parse(Count.Text) - 1).ToString();
+            }
+            //if it's not the subtract button calling the function, it must be the add button
+            else
+            {
+                //convert the count text from string to int, add 1, then convert back to string
+                Count.Text = (int.Parse(Count.Text) + 1).ToString();
+            }
+
+            //calculate and display total attendence 
+            TotalCountLabel.Text = Total();
+        }
+
+
+        /// <summary>
+        /// //calculate the total attendence 
+        /// </summary>
+        /// <returns>total attendance</returns>
+        private string Total()
+        {
+            //initialize totalcount
+            int TotalCount = 0;
+
+            //iterate thru every "Count" textbox
+            for (int j = 1; j <= i; j++)
+            {
+                //use int j to find "count" textbox with the matching index attached to its name
+                TextBox PartialCount = (TextBox)flowLayoutPanel1.Controls.Find($"Count { j }", false).FirstOrDefault();
+                //if there is invalid input in the count box, reset the value and notify the user
+                InputCheck(PartialCount);
+                //convert the text in the count box to an int
+                int PartialCountInt = int.Parse(PartialCount.Text);
+                //add the count of each bands attenddees to the total count
+                TotalCount += PartialCountInt;
+            }
+            //convert to string and return message "Total count: ..."
+            return $"Total count: { TotalCount.ToString()} ";
+        }
+
+        /// <summary>
+        /// if there is invalid input in the count box, reset the value and notify the user
+        /// returns true if input is valid, and false if input is not valid
+        /// </summary>
+        /// <param name="TB">The TextBox to be checked</param>
+        private bool InputCheck(TextBox TB)
+        {
+            try
+            {
+                int.Parse(TB.Text);
+            }
+            catch
+            {
+                MessageBox.Show($"*NOTICE* '{TB.Text}' is not valid input. When you click 'OK', this count will be reset to its last valid value");
+                TB.Undo();
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+
+
+}
